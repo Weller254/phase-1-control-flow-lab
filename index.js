7b6131e1b5d0c924f3fe2741dda27f38070b939a
Kelvin Muturi <muturikelvin893@gmail.com>
2023-03-14 19:35:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+let ride;
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride<=400){
+    return ("This one is on me!");
+  }
+  else if (ride>2000 && ride < 2500){
+    return ("I will gladly take your thirty bucks.");
+  }
+  else (ride<=2500);{
+    return ("No can do.");
+    
+  }
+  
+ 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city==="NYC"){
+    return ("Ok, sounds good.");
+  }
+  else if (city!=="NYC"){
+    return ("No go.");
+  }
+
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  if ("the tip is generous"){
+    return ("Thank you so much.");
+  }
+  if ("the tip is not as generous");{
+     return ("Thank you.");
+  }
+   if ("anything else"){
+  return ("Bye.")
+}
 }
